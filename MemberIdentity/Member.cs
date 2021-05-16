f664c33101ec77bcd01058d6f3a7c4420a475ad2
Lior Matsliah <liormatsliah1985@gmail.com>
2021-05-16 11:01:32 +0300

    Add project files.

---
+++ MemberIdentity/Member.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace WeAreFamily.Models.MemberIdentity
+{
+    [Keyless]
+    public class Member
+    {
+        [Key]
+        public int MembershipId { get; set; }
+        public int NationalityId { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
